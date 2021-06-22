3efd83f9a9cce00f65c86c51aef4653d79e81614
Riz <80712058+rizznn@users.noreply.github.com>
2021-06-22 03:21:06 -0400

    finish portfolio HTML output-update template function, about and project section HTML

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML - generatePage(name, github);
-
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-  
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-//   });
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     // returning what it returns, which is a Promise.
@@ -143,5 +134,11 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+    
+        console.log('Page created! Check out index.html to see the output!');
+      });
 });
